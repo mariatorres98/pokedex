81dd90d0daf596be95f886e218675ebbf91c11eb
Jennifer Parra <jspn48@gmail.com>
2022-09-26 22:13:23 -0500

    Add style and card print

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
-import { btnSearch, handleSearch } from "./scripts/ui.js";
+import { btnSearch, handleSearch, printPokemons } from "./scripts/ui.js";
 
 btnSearch.addEventListener('click', handleSearch);
 
 
 import { getinfo } from "./scripts/process.js";
-import {printPokemons} from "./scripts/ui.js";
-
-
 
 const mainFuntion= async () => {
     let data = await getinfo();
--- scripts/ui.js
+++ scripts/ui.js
@@ -22,16 +22,19 @@ export const printPokemons = (listPokemons) => {
 const input = document.getElementById('search');
 export const btnSearch = document.getElementById('btnSearch');
 
+
 const filterArray = async (word) => {
     const URL='https://pokeapi.co/api/v2/pokemon/';
     const { data : {results} } = await axios.get(URL);
     const searchFilter = results.filter(results => results.name.toLowerCase().includes(word.toLowerCase()));
     console.log(searchFilter);
+    printPokemons(searchFilter);
 };
 
 export const handleSearch = () => {
     let searchType = input.value;
     filterArray(searchType);
+    
 }
 
 
